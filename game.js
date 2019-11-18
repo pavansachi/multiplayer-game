893b2b8a6d70cc21c50ff538c43b867f833fa70a
pavansachi <pavan8sachi@gmail.com>
2019-11-19 07:15:14 +1100

    sprite cannot move out of game arena

--- game.js
+++ game.js
@@ -2,10 +2,11 @@ class Arena {
 
     sprites = [];
 
-    constructor(context, width, height) {
+    constructor(canvas) {
+        var context = canvas.getContext('2d');
         this.ctx = context;
-        this.width = width;
-        this.height = height;
+        this.width = canvas.width;
+        this.height = canvas.height;
     }
 
     add(sprite) {
@@ -17,8 +18,7 @@ class Arena {
         this.ctx.clearRect(0, 0, this.width, this.height);
 
         this.sprites.map(sprite => {
-            sprite.draw(this.ctx);
-            sprite.update(this.width, this.height);
+            sprite.draw(this.ctx, this.width, this.height);
         });
 
         requestAnimationFrame(this.draw.bind(this));
@@ -103,23 +103,59 @@ class Tank {
         window.addEventListener('keyup', this.controller.keyUp.bind(this.controller), false);
     }
 
-    draw(ctx) {
+    draw(ctx, width, height) {
 
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false);
         ctx.fillStyle = 'blue';
         ctx.fill();
         ctx.closePath();
+
+        this.update(width, height);
     }    
 
     update(width, height) {
 
         console.log("update");
 
-        if (this.controller.isRight()) this.x += this.dx;
-        if (this.controller.isLeft()) this.x -= this.dx;
-        if (this.controller.isUp()) this.y += this.dy;
-        if (this.controller.isDown()) this.y -= this.dy;
+        if (this.controller.isRight()) {
+
+            if (this.x + this.dx > width - this.size) {
+
+            }
+            else {
+                this.x += this.dx;
+            }
+        }
+
+        if (this.controller.isLeft()) {
+
+            if (this.x + this.dx < this.size) {
+
+            }
+            else {
+                this.x -= this.dx;
+            }
+        }
+
+        if (this.controller.isUp()) {
+            if (this.y + this.dy < this.size) {
+
+            }
+            else {
+                this.y += this.dy;
+            }
+        }
+
+        if (this.controller.isDown()) {
+            
+            if (this.y + this.dy > height - this.size) {
+
+            }
+            else {
+                this.y -= this.dy;
+            }
+        }
     }
 
 }
@@ -128,12 +164,11 @@ window.onload = () => {
     console.log("window loaded");
 
     var canvas = document.getElementById('arena');
-    var context = canvas.getContext('2d');
    
     var cX = canvas.width / 2;
     var cY = canvas.height - 30;
 
-    const arena = new Arena(context, canvas.width, canvas.height);
+    const arena = new Arena(canvas);
     const tank = new Tank(cX, cY);
 
     arena.add(tank);
